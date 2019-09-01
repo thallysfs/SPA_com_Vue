4cd4781b9b41dcb8a1a3f0325e6857e0fe607616
thallysfs <50297870+thallysfs@users.noreply.github.com>
2019-09-01 12:47:38 -0300

    componente footer, grid e CardMenu

---
+++ social/src/components/layouts/GridVue.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="'col s' + tamanho">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GridVue',
+  props:['tamanho'],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Adicione "Scoped" atributo limita o CSS apenas nesse componente  -->
+<style scoped>
+
+</style>
---
+++ social/src/components/layouts/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <nav :class="cor || 'blue'">
+      <div class="nav-wrapper container">
+        <a :href="url || '#'" class="brand-logo">{{logo || 'Site'}}</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="">sass</a></li>
+          <li><a href="">sass <span class="new badge">4</span></a></li>
+          <li><a href="">sass</a></li>
+        </ul>
+      </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props:['logo', 'url', 'cor'],
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Adicione "Scoped" atributo limita o CSS apenas nesse componente  -->
+<style scoped>
+
+</style>
