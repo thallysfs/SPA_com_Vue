d5a352819c16351b84730ad32255956e763547d0
thallysfs <50297870+thallysfs@users.noreply.github.com>
2019-09-05 21:59:11 -0300

    ajustes nas urls, tirando o "#", trabalhando com o history mode

--- social/src/components/layouts/NavBar.vue
+++ social/src/components/layouts/NavBar.vue
@@ -1,7 +1,8 @@
 <template>
     <nav :class="cor || 'blue'">
       <div class="nav-wrapper container">
-        <a :href="url || '#/'" class="brand-logo">{{logo || 'Site'}}</a>
+        <!-- <a :href="url || '#/'" class="brand-logo">{{logo || 'Site'}}</a> abaixo segue router-link no logar do "a" -->
+        <router-link class="brand-logo" :to="url || '/'">{{logo || 'Site'}}</router-link>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
           <slot />
         </ul>
