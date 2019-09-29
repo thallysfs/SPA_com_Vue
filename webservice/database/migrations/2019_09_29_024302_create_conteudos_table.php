37b855f64127d290ac53f822c451068272dde428
thallysfs <50297870+thallysfs@users.noreply.github.com>
2019-09-29 00:01:01 -0300

    Criando as tabelas e modelos

---
+++ webservice/database/migrations/2019_09_29_024302_create_conteudos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConteudosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conteudos', function (Blueprint $table) {
+            $table->increments('id');
+            //craindo chave estrangeira da tabela "users"
+            $table->unsignedBigInteger('user_id');
+            //associando a chave estrangeira da tabela "users" - onDelete irá apagar todos os conteúdos se o usuário id desse contaúdo for apagado
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('titulo');
+            $table->longText('texto');
+            $table->string('imagem');
+            $table->string('link');
+            $table->dateTime('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conteudos');
+    }
+}
---
+++ webservice/database/migrations/2019_09_29_024358_create_comentarios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComentariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comentarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('conteudo_id');
+            $table->foreign('conteudo_id')->references('id')->on('conteudos')->onDelete('cascade');
+            $table->longText('texto');
+            $table->dateTime('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comentarios');
+    }
+}
